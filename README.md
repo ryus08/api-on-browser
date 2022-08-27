e81b5eff811ab4aa48bb478727cef20209c72c2f
Zachary Marois <zachary.marois@gmail.com>
2022-08-27 18:24:03 -0400

    correctly add and remove rules

--- README.md
+++ README.md
@@ -4,14 +4,12 @@ Provides a browser extension for navigating APIs like normal web navigation
 ## TODO:
 - Attribute the icon: <a href="https://www.flaticon.com/free-icons/api" title="api icons">Api icons created by Freepik - Flaticon</a>
 - Correct the icon image sizes, I don't think Google will allow upload if they aren't right.
-- Add/remove auth rules
+- Load rules on start, assuming the session settings mean they aren't persisting
 - Load auth rules from file/S3/url
 - Load auth parameters from different file formats, merge as jsom json path to extract from the merge
 - More-complex rule builder
 - Verify security of rules. Can other extensions read what is written? Is it persisted to disk? I believe the doc suggested there were two types, one kept only in memory. API docs suggest the rule ids are scoped to the extension
 - Figure out if the user can add host permissions. Right now set to extremely broad access, would like the user to be able to say which are allowed as they configure their rules. [Chromium docs](https://chromium.googlesource.com/chromium/src/+/main/extensions/docs/permissions.md#Runtime-Host-Permissions) seems to suggest its an option, maybe can add just for Edge/Opera/etc. ActiveTab is grants some of it, but the extension needs to be "invoked" before it's enabled, meaning every navigation would need the user to say explicitly "k, you can run the rules on this site". Better than nothing. If this is the avenue we go down, explore other [options for invoking the extension more seamlessly](https://developer.chrome.com/docs/extensions/mv3/manifest/activeTab/#invoking-activeTab)
-- Messed up storage hook. Figure it out
-- Need a better id field both for render, and the declarative network request rule
 - Maybe want independent updates for each rule. Right now the whole array is updated. 
 - Need to update the rule if it doesn't exist
     - check if other extensions can interfere with the id space
--- src/options/Rules.tsx
+++ src/options/Rules.tsx
@@ -1,24 +1,43 @@
 import React from 'react';
+import {
+  Button, Container, Row,
+} from 'react-bootstrap';
 import CardGroup from 'react-bootstrap/CardGroup';
 import { Rule as RuleSettings } from '../domain/Rule';
 import useChromeStorage from '../hooks/useChromeStorage';
 import Rule from './Rule';
 
 export default function Rules() {
-  const [rules, setRules] = useChromeStorage<RuleSettings[]>('rules', [{ id: 1, domain: '', authStrategy: '' }]);
-  const rule = rules[0];
+  const [rules, setRules] = useChromeStorage<RuleSettings[]>('rules', []);
   return (
     <div>
       <CardGroup>
-        {rule && (
-        <Rule
-          // key={rule.id} // Getting id into storage seems to break the storage loading into the component
-          rule={{ ...rule }}
-          removeRule={() => setRules([{ id: 1, domain: '', authStrategy: '' }])}
-          setRule={(updatedRule) => setRules([{ ...updatedRule }])}
-        />
-        )}
-
+        <Container>
+          {rules.map((rule) => (
+            <Row key={rule.id}>
+              <Rule
+                rule={{ ...rule }}
+                removeRule={() => setRules(rules.filter((x) => x.id !== rule.id))}
+                setRule={(updatedRule) => setRules(rules.map((x) => {
+                  if (x.id !== rule.id) {
+                    return x;
+                  }
+                  return { ...updatedRule };
+                }))}
+              />
+            </Row>
+          ))}
+          <Row>
+            <Button onClick={() => {
+              const nextId = Array.from({ length: rules.length + 1 }, (_, i) => i + 1)
+                .find((i) => !rules.map((x) => x.id).includes(i));
+              return setRules([...rules, { id: nextId, authStrategy: '', urlFilter: '' }]);
+            }}
+            >
+              <i className="bi bi-plus-circle" />
+            </Button>
+          </Row>
+        </Container>
       </CardGroup>
     </div>
   );
